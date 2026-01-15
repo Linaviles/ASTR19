3c5b55fdf8a58c5d7ffe390860343dd9db70d5dc
Lindsay Aviles Brizuela <linavilesb@gmail.com>
2026-01-14 19:35:36 -0800

    Add files via upload

---
+++ prompt2.py
@@ -0,0 +1,14 @@
+x = 8.8
+y = 1.5
+z = 5
+
+add = x+y 
+sub = x-y
+mult = x * z
+
+print("8.8 + 1.5 = ", add)
+print("This is the type of the sum ->",type(add))
+print("8.8 - 1.5 = ", sub)
+print("This is the type of the difference ->",type(sub))
+print("8.8 * 5 = ", mult)
+print("This is the type of the product ->",type(mult))
